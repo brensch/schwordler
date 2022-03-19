58c53a4007dc42836daf32a596ed5f5e534d8c3a
brensch <brendolini@protonmail.com>
2022-03-18 20:12:54 -0700

    tester will iterate through all words and then compress the output

--- tester/main.go
+++ tester/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"compress/gzip"
 	"encoding/json"
 	"fmt"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/brensch/battleword"
@@ -11,73 +13,133 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-func main() {
-	s := schwordler.InitStore(logrus.New())
-	numberGuesses := len(battleword.CommonWords)
-	allResults := make([][]battleword.GuessResult, numberGuesses)
-
-	start := time.Now()
-	j := 0
-	for i, answer := range battleword.CommonWords {
-		j++
-		if j > numberGuesses {
-			break
-		}
-		var results []battleword.GuessResult
-
-		for {
-
-			guess, err := s.GuessWord(results)
-			if err != nil {
-				fmt.Println(err)
-				break
-			}
-
-			result := battleword.GetResult(guess, answer)
-			results = append(results, battleword.GuessResult{
-				Guess:  guess,
-				Result: result,
-			})
-
-			if guess == answer {
-				allResults[i] = results
-				// fmt.Println("guessed it", answer)
-				break
-			}
-
-			if len(results) > 5 {
-				allResults[i] = append(results, battleword.GuessResult{ID: "failed", Guess: answer})
-				fmt.Println("failed", answer)
-				break
-			}
-		}
-	}
-
-	end := time.Now()
-
-	fmt.Printf("finished in %f seconds\n", end.Sub(start).Seconds())
-	fs, err := os.Create("guess.json")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer fs.Close()
-
-	err = json.NewEncoder(fs).Encode(allResults)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	scoreList(allResults)
+type Result struct {
+	Starter string        `json:"starter,omitempty"`
+	Time    time.Duration `json:"time,omitempty"`
+	Average float64       `json:"average,omitempty"`
 }
 
-func scoreList(input [][]battleword.GuessResult) {
+func main() {
+	s := schwordler.InitStore(logrus.New())
+
+	concurrentSolverCHAN := make(chan struct{}, 6)
+	resultCHAN := make(chan Result)
+	var wgGenerator, wgListener sync.WaitGroup
+
+	wgListener.Add(1)
+	go func() {
+		var allResults []Result
+		lowestAverage := float64(6)
+		var lowestAverageWord string
+
+		defer wgListener.Done()
+		for result := range resultCHAN {
+			allResults = append(allResults, result)
+			if result.Average < lowestAverage {
+				lowestAverage = result.Average
+				lowestAverageWord = result.Starter
+			}
+			fmt.Printf("finished %s in %f seconds with average %f\n", result.Starter, result.Time.Seconds(), result.Average)
+
+		}
+
+		fs, err := os.Create("./result.json")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer fs.Close()
+
+		err = json.NewEncoder(fs).Encode(allResults)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Println("best word is", lowestAverageWord)
+
+	}()
+
+	for _, starter := range battleword.CommonWords {
+		wgGenerator.Add(1)
+		go func(starter string) {
+			allResults := make([][]battleword.GuessResult, len(battleword.CommonWords))
+			defer wgGenerator.Done()
+
+			concurrentSolverCHAN <- struct{}{}
+			defer func() { <-concurrentSolverCHAN }()
+
+			fmt.Printf("starting %s\n", starter)
+			start := time.Now()
+
+			for i, answer := range battleword.CommonWords {
+				// do the first result from the current start word.
+				var results []battleword.GuessResult
+
+				for {
+					guess, err := s.GuessWord(results, starter)
+					if err != nil {
+						fmt.Println(err)
+						break
+					}
+
+					result := battleword.GetResult(guess, answer)
+					results = append(results, battleword.GuessResult{
+						Guess:  guess,
+						Result: result,
+					})
+
+					if guess == answer {
+						allResults[i] = results
+						// fmt.Println("guessed it", answer)
+						break
+					}
+
+					if len(results) > 5 {
+						allResults[i] = append(results, battleword.GuessResult{ID: "failed", Guess: answer})
+						// fmt.Println("failed", answer)
+						break
+					}
+				}
+			}
+
+			end := time.Now()
+
+			resultCHAN <- Result{
+				Starter: starter,
+				Time:    end.Sub(start),
+				Average: scoreList(allResults),
+			}
+
+			fs, err := os.Create(fmt.Sprintf("./deepdive/%s.json.gz", starter))
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			defer fs.Close()
+
+			gz := gzip.NewWriter(fs)
+			defer gz.Close()
+
+			err = json.NewEncoder(gz).Encode(allResults)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+		}(starter)
+	}
+
+	wgGenerator.Wait()
+	close(resultCHAN)
+	wgListener.Wait()
+
+}
+
+func scoreList(input [][]battleword.GuessResult) float64 {
 	var length int
 	for _, guessResult := range input {
 		length += len(guessResult)
 	}
-	fmt.Println("total length", length)
-	fmt.Println("total input length", len(input))
-	fmt.Println("average length", float64(length)/float64(len(input)))
+
+	return float64(length) / float64(len(input))
 }
